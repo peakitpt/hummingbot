d2be3cf3dc56f55dc07a6b4380c028525321ecd2
tiago-pinto <46755718+tiago-pinto@users.noreply.github.com>
2024-04-18 15:14:01 +0100

    Revert rebase w/fixes

--- controllers/generic/grid.py
+++ controllers/generic/grid.py
@@ -49,7 +49,6 @@ class GridConfig(ControllerConfigBase):
             prompt=lambda mi: "Enter initial margin($150): ",
             prompt_on_new=True))
   
-  
     executor_refresh_time: int = Field(
         default=60, gt=0,
         client_data=ClientFieldData(
@@ -154,17 +153,15 @@ class GridController(ControllerBase):
             actions.extend(self.stop_actions_proposal())
         else:
             actions.extend(self.panic_close_all())
+        return actions
 
     def panic_close_all(self) -> List[StopExecutorAction]:
         stop_actions = []
         all_executors = self.get_all_executors()
         active_positions = self.filter_executors(executors=all_executors, filter_func=lambda x: x.is_active)
-        
         for executor in active_positions:
             stop_actions.append(StopExecutorAction(controller_id = self.config.id, executor_id = executor.id))
-        
         return stop_actions
-        
         
     async def update_processed_data(self):
         reference_price = self.market_data_provider.get_price_by_type(self.config.connector_name, self.config.trading_pair, PriceType.MidPrice)
@@ -172,7 +169,7 @@ class GridController(ControllerBase):
 
     def get_all_executors(self) -> List[ExecutorInfo]:
         return self.filter_executors(executors = self.executors_info, filter_func = lambda x: x.trading_pair == self.config.trading_pair and x.side == TradeType.BUY and x.type == "position_executor")
-    
+
     def get_entry_price(self, update_file = False) -> Decimal:
         entry_price = 0
         if self.config.entry_price > 0:
@@ -193,7 +190,7 @@ class GridController(ControllerBase):
             initial_margin = round(not_trading_positions[0].config.amount * self.get_entry_price() * self.config.levels / self.config.leverage)
             # INITIAL MARGIN CHANGED, STOP THE POSITIONs (System should create new ones)
             if (self.config.initial_margin + self.config.reinvested) != initial_margin:
-                self.logger().info(f"Initial margin changed from {initial_margin} to {self.config.initial_margin} ")
+                self.logger().info(f"Initial margin changed from {initial_margin} to {self.config.initial_margin + self.config.reinvested} ")
                 for i in range(0, len(not_trading_positions), 1):
                     actions.append(StopExecutorAction(controller_id = self.config.id, executor_id = not_trading_positions[i].id))
 
@@ -205,7 +202,7 @@ class GridController(ControllerBase):
         return self.get_level_entry_price(level_id-1) * Decimal(1 - self.config.step)
 
     def create_actions_proposal(self) -> List[CreateExecutorAction]:
-        create_actions = []        
+        create_actions = []
         current_tick_count = int(time.time()) - self.start_time
 
         all_executors = self.get_all_executors()
@@ -221,7 +218,7 @@ class GridController(ControllerBase):
 
         create_actions.extend(self.make_trailing_actions(active_positions))
         return create_actions
-        
+    
     def make_missing_levels_actions(self, active_positions) -> List[CreateExecutorAction]:        
         actions = []
         # Check if there is at least one executor per each level
@@ -234,7 +231,6 @@ class GridController(ControllerBase):
                     actions.append(CreateExecutorAction(controller_id = self.config.id, executor_config = self.get_executor_config(self.get_level_entry_price(i), order_amount, i)))
         
         return actions
-        
     
     
     def make_trailing_actions(self, active_positions) -> List[CreateExecutorAction]:
@@ -263,8 +259,8 @@ class GridController(ControllerBase):
     def stop_actions_proposal(self) -> List[StopExecutorAction]:
         stop_actions = []
         all_executors = self.get_all_executors()
-        active_executors = self.filter_executors(executors=all_executors, filter_func=lambda x: x.is_active)        
-        
+        active_executors = self.filter_executors(executors=all_executors, filter_func=lambda x: x.is_active)
+
         stop_actions.extend(self.make_trailing_stop_actions(active_executors))
         stop_actions.extend(self.check_if_positions_match_initial_margin(active_executors = active_executors))
 
